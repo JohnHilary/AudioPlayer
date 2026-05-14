60bf60690afb80d0322b4ec26676e97718ac89ed
John Hilary <john.h@codelynks.com>
2026-05-14 13:43:45 +0530

    codeCleanup:UiState provide removal

--- app/src/main/java/com/john/audioplayer/di/AppModule.kt
+++ app/src/main/java/com/john/audioplayer/di/AppModule.kt
@@ -2,13 +2,11 @@ package com.john.audioplayer.di
 
 import android.content.Context
 import com.john.audioplayer.audio.AudioPlayerManager
-import com.john.audioplayer.state.AudioPlayerScreenUiState
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Singleton
 
 
@@ -25,9 +23,4 @@ object AppModule {
         return AudioPlayerManager(context)
     }
 
-    @Provides
-    @Singleton
-    fun provideAudioPlayerStateFlow(): MutableStateFlow<AudioPlayerScreenUiState> {
-        return MutableStateFlow(AudioPlayerScreenUiState())
-    }
 }
--- app/src/main/java/com/john/audioplayer/navigation/NavigationHost.kt
+++ app/src/main/java/com/john/audioplayer/navigation/NavigationHost.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
